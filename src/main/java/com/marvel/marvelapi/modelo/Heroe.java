2fcc49a3dc53f1202e1bfabdc51bcbdcc605e840
Alberto <albantunez91@gmail.com>
2023-03-07 15:00:46 +0000

    Third Commit.

--- src/main/java/com/marvel/marvelapi/modelo/Heroe.java
+++ src/main/java/com/marvel/marvelapi/modelo/Heroe.java
@@ -1,12 +1,18 @@
 package com.marvel.marvelapi.modelo;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
-@Table
+@Table(name = "heroe")
+@NoArgsConstructor
+@Data
 public class Heroe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
     @Column(nullable = false)
     private String nombre;
