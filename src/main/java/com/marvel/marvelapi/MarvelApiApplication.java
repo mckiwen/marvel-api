c7b6766457548eca284765421903461df1af0e0c
Alberto <albantunez91@gmail.com>
2023-03-09 02:30:16 +0000

    Commit 15: Tests performed.

--- src/main/java/com/marvel/marvelapi/MarvelApiApplication.java
+++ src/main/java/com/marvel/marvelapi/MarvelApiApplication.java
@@ -1,12 +1,9 @@
 package com.marvel.marvelapi;
 
-import com.marvel.marvelapi.modelo.Quote;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class MarvelApiApplication {
